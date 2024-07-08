fa085895f23393eefc3f5c9b40c0a21980d87d07
shunmakk <shunmakajioka12@gmail.com>
2024-07-09 02:18:26 +0900

    マーカーを立てる

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ function initMap() {
     }
     // マップの初期化処理を行う
     const map = new google.maps.Map(mapElement, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        center: {
+            lat: -34.397, //経度
+            lng: 150.644 //緯度
+        },
+        zoom: 2
     });
+
+    // マーカーの追加
+    const marker = new google.maps.Marker({
+        position: { lat: -34.397, lng: 150.644 }, // マーカーを立てる位置の指定
+      map: map // マーカーを立てる地図の指定
+   });
 }
