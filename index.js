4f602b72be7638bac23125b15a3aefd7def4015b
shunmakk <shunmakajioka12@gmail.com>
2024-07-09 03:39:49 +0900

    マーカーの吹き出しを追加とGoogle Maps APIのコールバック関数を指定

--- index.js
+++ index.js
@@ -18,4 +18,18 @@ function initMap() {
         position: { lat: 35.7356, lng: 139.6516 }, // マーカーを立てる位置の指定
       map: map // マーカーを立てる地図の指定
    });
+
+
+   // 吹き出しの内容を設定
+   const infoWindow = new google.maps.InfoWindow({
+    content: '<div>練馬区役所</div>'
+    });
+
+// マーカーをクリックしたときに吹き出しを表示する
+marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+});
 }
+
+// Google Maps APIのコールバック関数を指定
+window.initMap = initMap;
